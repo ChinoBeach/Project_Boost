4c513d531d1382ceac3be7073fa16d9c7f9538f7
ChinoBeach <beach988@morrisville.edu>
2022-09-12 23:00:50 -0400

    Introducing Classes

---
+++ Project_Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input. This class already has these methods/functions because it inherited them from MonoBehaviour
+    }
+}
