80dfdc766f4c0c7df2f89e75b98dc829d62182c1
ChinoBeach <beach988@morrisville.edu>
2022-09-20 22:46:47 -0400

    Respawning Using SceneManager

--- Project_Boost/Assets/Scripts/CollisionHandler.cs
+++ Project_Boost/Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -10,24 +11,44 @@ public class CollisionHandler : MonoBehaviour
         {
             //if the player runs into something tagged as friendly
             case "Friendly":
+
+                //print message
                 Debug.Log("This this is friendly");
                 break;
 
             //if the player runs into something tagged as Finish
             case "Finish":
+
+                //print message
                 Debug.Log("Level Finished");
                 break;
 
             //if the player runs into something tagged as Fuel
             case "Fuel":
+
+                //print message
                 Debug.Log("Fuel Aquired");
                 break;
 
             //if the player runs into something that isnt tagged, or doesnt match a tag above
             default:
+
+                //print message
                 Debug.Log("Explosion Detetect. Ship Lost.");
+
+                //reload the level
+                ReloadLevel();
                 break;
         }
        
+    }
+
+    void ReloadLevel()
+    {
+        //current Scene 
+        int intCurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        //reload the scene
+        SceneManager.LoadScene(intCurrentSceneIndex);
     }
 }
