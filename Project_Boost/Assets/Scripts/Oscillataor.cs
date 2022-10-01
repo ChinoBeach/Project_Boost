129e7309c30b9b1221d1089f200f71ada60754d7
ChinoBeach <beach988@morrisville.edu>
2022-10-01 13:50:42 -0400

    Mathf.Sin() For Oscillation

--- Project_Boost/Assets/Scripts/Oscillataor.cs
+++ Project_Boost/Assets/Scripts/Oscillataor.cs
@@ -8,8 +8,8 @@ public class Oscillataor : MonoBehaviour
     Vector3 vectorStartingPos;                               // starting position of object
 
     [SerializeField]Vector3 vectorMovement;                  // how much we want it to move out
-    [SerializeField] [Range(0,1)]float  fltMovementFactor;   // oscioation control
-
+    float  fltMovementFactor;   // oscioation control
+    [SerializeField] float fltPeriod = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +21,14 @@ public class Oscillataor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //variables
+        const float fltTau = Mathf.PI * 2;                      //contastnt value of 6.283
+
+        float fltCycles = Time.time / fltPeriod;                //contilly growing over time
+        float fltRawSinWave = Mathf.Sin(fltCycles * fltTau);    //going from -1 to 1
+
+        fltMovementFactor = (fltRawSinWave + 1f) / 2f;          //recaulcutaed to go from 0 to 1
+
         //set the offset
         Vector3 offset = vectorMovement * fltMovementFactor;
 
